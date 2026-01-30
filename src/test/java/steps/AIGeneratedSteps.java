c854712eb2ddf80108e77b5bc12e9608fa7811dd
sanu1s <arjunkrishnansuresh@gmail.com>
2026-01-29 23:46:27 -0800

    AI generated Cucumber test cases for Jira requirements

--- src/test/java/steps/AIGeneratedSteps.java
+++ src/test/java/steps/AIGeneratedSteps.java
@@ -23,32 +23,32 @@ public class AIGeneratedSteps {
 
 @When("User clicks the {string} button")
 public void userClicksTheButton(String buttonText) {
-    System.out.println("Clicking button: " + buttonText);
+    System.out.println("Executing: User clicks the " + buttonText + " button");
     page.locator("button:has-text('" + buttonText + "')").click();
 }
 
 @Given("User is on the order tracking page")
-public void userIsOnOrderTrackingPage() {
-    System.out.println("Navigating to order tracking page");
-    page.goto("http://127.0.0.1:8088/order/tracking");
+public void userIsOnTheOrderTrackingPage() {
+    System.out.println("Executing: User is on the order tracking page");
+    page.navigate("http://127.0.0.1:8088/order/tracking");
     assertThat(page).hasURL("http://127.0.0.1:8088/order/tracking");
 }
 
 @When("User enters order number {string}")
 public void userEntersOrderNumber(String orderNo) {
-    System.out.println("Entering order number: " + orderNo);
+    System.out.println("Executing: User enters order number " + orderNo);
     page.locator("#order_no").fill(orderNo);
 }
 
 @Then("The error message should display {string}")
 public void theErrorMessageShouldDisplay(String expectedError) {
-    System.out.println("Verifying error message: " + expectedError);
+    System.out.println("Executing: The error message should display " + expectedError);
     assertThat(page.locator(".result")).containsText(expectedError);
 }
 
 @Then("The order status should display {string}")
 public void theOrderStatusShouldDisplay(String expectedStatus) {
-    System.out.println("Verifying order status: " + expectedStatus);
+    System.out.println("Executing: The order status should display " + expectedStatus);
     assertThat(page.locator(".result")).containsText(expectedStatus);
 }
 
