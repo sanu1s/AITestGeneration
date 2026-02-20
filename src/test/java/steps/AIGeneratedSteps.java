fbbf908dcd2e1e0c0e3b077ab24717f6d36ef507
sanu1s <arjunkrishnansuresh@gmail.com>
2026-02-19 20:54:32 -0800

    AI generated Cucumber test cases for Jira requirements

--- src/test/java/steps/AIGeneratedSteps.java
+++ src/test/java/steps/AIGeneratedSteps.java
@@ -21,28 +21,34 @@ public class AIGeneratedSteps {
         this.page = testContext.page;
     }
 
-@When("User clicks the {string} button")
-public void userClicksTheButton(String buttonText) {
-    System.out.println("Clicking button: " + buttonText);
-    page.locator("button:has-text('" + buttonText + "')").click();
-}
-
 @Given("User is on the order tracking page")
-public void userIsOnOrderTrackingPage() {
+public void userIsOnTheOrderTrackingPage() {
     System.out.println("Navigating to order tracking page");
     page.navigate("http://localhost:8088/order/tracking");
 }
 
-@Then("The status message should display {string}")
-public void theStatusMessageShouldDisplay(String expectedMessage) {
-    System.out.println("Verifying status message: " + expectedMessage);
+@Then("The order status result should display {string}")
+public void theOrderStatusResultShouldDisplay(String expectedMessage) {
+    System.out.println("Verifying order status result: " + expectedMessage);
     assertThat(page.locator(".result")).containsText(expectedMessage);
+}
+
+@When("User clicks the Track Order button")
+public void userClicksTheTrackOrderButton() {
+    System.out.println("Clicking Track Order button");
+    page.locator("button:has-text('Track Order')").click();
 }
 
 @When("User enters order number {string}")
 public void userEntersOrderNumber(String orderNo) {
     System.out.println("Typing order number: " + orderNo);
     page.locator("#order_no").fill(orderNo);
+}
+
+@Then("The error message should display {string}")
+public void theErrorMessageShouldDisplay(String expectedErrorMessage) {
+    System.out.println("Verifying error message: " + expectedErrorMessage);
+    assertThat(page.locator(".result")).containsText(expectedErrorMessage);
 }
 
 }
